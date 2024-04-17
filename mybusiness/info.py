df188b0de27a87b3481d8a7295b5e703f03fdb93
veekta <vsunday.1710@gmail.com>
2024-04-17 15:53:19 +0100

    new commit

---
+++ mybusiness/info.py
@@ -0,0 +1,6 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'onyekachukwu4life2000@gmail.com'
+EMAIL_HOST_PASSWORD = 'txvy cydf jrjd wfxn'
+EMAIL_PORT = 587
+'vsunday.1710@gmail.com'
